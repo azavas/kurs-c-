ef7dc19e2c793c80e89579114ec3eeaaae4e8687
Anna <azavas@yandex.ru>
2022-10-03 09:43:01 +0300

    Решили 1 задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя: ");
+string name = Console.ReadLine ();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Квадрат {number} равен {number * number}");
+Console.WriteLine("Ваше имя "+name);
